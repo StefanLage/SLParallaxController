143264808caafdb52a061f6f68e7fb6725f7136a
Stefan Lage <lagestfan@gmail.com>
2014-03-15 00:09:37 +0100

    Init commit

---
+++ SLParallaxController/SLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SLAppDelegate.h
+//  SLParallaxController
+//
+//  Created by Stefan Lage on 15/03/14.
+//  Copyright (c) 2014 Stefan Lage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SLParallaxController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SLParallaxController
+//
+//  Created by Stefan Lage on 15/03/14.
+//  Copyright (c) 2014 Stefan Lage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SLAppDelegate class]));
+    }
+}
